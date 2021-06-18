fe09e81d13963bdb8c3b358a7f80b9f8c5dccf26
Muhammad Atif <53166969+Muhammad-Atif-Ch@users.noreply.github.com>
2021-06-18 09:28:00 +0500

    Update Delete Button Sweet Alert.php

--- .github/workflows/Delete Button Sweet Alert.php
+++ .github/workflows/Delete Button Sweet Alert.php
@@ -1,6 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <title>Brogrammer Team Ecommerce</title>
+    <!-- GLOBAL MAINLY STYLES-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossorigin="anonymous">
+    <!-- Delete Sweet Alert -->
+    <link rel="stylesheet" href="https://sweetalert2.github.io/styles/bootstrap4-buttons.css">
+</head>
+<body>
+
+
+
+
 <a href="" id="delete" class="btn btn-danger">Delete</a>
 
-
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script>
 $(document).on("click", "#delete", function (e) {
         e.preventDefault();
         var link = $(this).attr("href");
@@ -38,4 +59,7 @@ swalWithBootstrapButtons.fire({
     )
   }
 })
-    });
+});
+</script>
+</body>
+</html>
